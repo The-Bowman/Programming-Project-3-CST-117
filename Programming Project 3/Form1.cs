ab16eb393f698701b3b451d6fd41108e61ee36fe
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-10-31 22:20:55 -0700

    added comments, debugged double file open and save

--- Programming Project 3/Form1.cs
+++ Programming Project 3/Form1.cs
@@ -20,9 +20,8 @@ namespace Programming_Project_3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            StreamReader fileSelected;
-            openFile.ShowDialog();
+            //setting up variables to process file
+            StreamReader fileSelected;          
             string lower;
             string first;
             string last;
@@ -32,35 +31,40 @@ namespace Programming_Project_3
             int vowels;
             int maxVowels = 0;
 
+            //determines if a file was selected to process
             if (openFile.ShowDialog() == DialogResult.OK)
             {
-                fileSelected = File.OpenText(openFile.FileName);
-                lower = fileSelected.ReadLine().ToLower();
-                string[] sorted = lower.Split(' ');
-                Array.Sort(sorted);
-                first = sorted[0];
-                last = sorted[sorted.Length - 1];
+                fileSelected = File.OpenText(openFile.FileName); //opens file
+                lower = fileSelected.ReadLine().ToLower(); //converts string to lower case
+                string[] sorted = lower.Split(' '); //splits string into an array, delimiting at each space between words
+                Array.Sort(sorted); //Array class method to sort the array alphabetically
+                first = sorted[0]; //stores first index of string array
+                last = sorted[sorted.Length - 1]; //stores last index of string array
                 int currentStringLength = 0;
+
+                //loops through each "word" in the array
                 foreach (string s in sorted)
                 {
-                    
+                    //checks length of string against highest, assigns new highest to the current word if it is longer
                     if (s.Length > currentStringLength)
                     {
                         longest = s;
                         currentStringLength = s.Length;
                     }
 
-                    vowels = 0;
+                    vowels = 0; //resets vowel count
 
+                    //loops through each character of the "word" checking for vowels
                     for (int i = 0; i < s.Length; i++)
                     {
                         
+                        //vowel check
                         if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
                         {
-                            vowels++;
+                            vowels++; //iterates vowels
                         }
 
-
+                        //checks if nuber of vowels of the current word is greater than the highest, assigns new string to highest vowel count if true
                         if (vowels > maxVowels)
                         {
                             maxVowels = vowels;
@@ -76,23 +80,26 @@ namespace Programming_Project_3
                 displayText.Text = "The contenst of the file in lower case reads:\n" + lower + "\n\n"+ "The first and last words, alphabetically:\n"  +"First: " + first +
                     " / Last: " + last + "\n\n" +"This is the longest word in the file:\n" + longest + ": The number of letters: " + currentStringLength + "\n\n" + 
                     "The word with the most vowels is:\n" + mostVowels;
-               
+
+                fileSelected.Close();
             } else
             {
                 MessageBox.Show("No file selected.");
             }
+
+            
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close(); //closes program
         }
 
         private void saveButton_Click(object sender, EventArgs e)
         {
             StreamWriter outputToFile;
-            saveFile.ShowDialog();
-
+            
+            //allows user to save file to their specified location
             if (saveFile.ShowDialog() == DialogResult.OK)
             {
                 try
